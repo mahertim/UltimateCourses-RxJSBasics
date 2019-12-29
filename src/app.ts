e470259ef95c96d954436fb72b7f1e6f57bae01b
Tim Maher <tim@timmaher.dev>
2019-12-29 10:11:56 -0500

    Emit items based on a duration with interval and timer

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,5 @@
-import { Observable, from } from 'rxjs';
+import { Observable, timer } from 'rxjs';
 
-const observer = {
-  next: (val: any): void => console.log('next', val),
-  error: (val: any): void => console.log('error', val),
-  complete: (): void => console.log('complete'),
-};
+const timer$: Observable<number> = timer(2000, 1000);
 
-function* hello() {
-  yield 'hello';
-  yield 'world';
-}
-const iterator = hello();
-const iteratorSource$ = from(iterator);
-iteratorSource$.subscribe(observer);
-console.log('----------');
-
-const arraySource$: Observable<number> = from([1, 2, 3, 4, 5]);
-arraySource$.subscribe(observer);
-console.log('----------');
-
-const stringSource$: Observable<string> = from('hello');
-stringSource$.subscribe(observer);
-console.log('----------');
-
-const promiseSource$: Observable<Response> = from(
-  fetch('https://api.github.com/users/octocat'),
-);
-promiseSource$.subscribe(observer);
+timer$.subscribe(console.log);

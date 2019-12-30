920d5191c3a4d444b6242327cc34346302f94fcc
Tim Maher <tim@timmaher.dev>
2019-12-30 14:55:38 -0500

    Lab 2: Build a Countdown Timer

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,26 @@
-import { from } from 'rxjs';
-import { scan, map } from 'rxjs/operators';
+import { interval } from 'rxjs';
+import { scan, mapTo, filter } from 'rxjs/operators';
 
-// const numbers = [1, 2, 3, 4, 5];
-class User {
-  constructor(
-    public name: string = '',
-    public loggedIn: boolean = false,
-    public token: string | null = null,
-  ) {}
-}
+// elements
+const countdown = document.getElementById('countdown');
+const message = document.getElementById('message');
 
-const users = [
-  new User('Richard', false, null),
-  new User('Gilfoil', true, 'abc'),
-  new User('Jared', true, '123'),
-];
+const counter$ = interval(1000);
 
-const state$ = from(users).pipe(
-  scan((accumulator, currentValue) => {
-    return { ...accumulator, ...currentValue };
-  }, new User()),
-);
-
-const name$ = state$.pipe(map(user => user.name));
-
-name$.subscribe(console.log);
+counter$
+  .pipe(
+    mapTo(-1),
+    scan((accumulator, current) => {
+      return accumulator + current;
+    }, 10),
+    filter(value => value >= 0),
+  )
+  .subscribe(value => {
+    console.log(value);
+    if (countdown instanceof HTMLElement) {
+      countdown.innerHTML = value.toString();
+    }
+    if (!value && message instanceof HTMLElement) {
+      message.innerHTML = 'Liftoff!';
+    }
+  });

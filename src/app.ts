79f90cc891c49d956c84183c6e1a593e46d4853c
Tim Maher <tim@timmaher.dev>
2019-12-31 09:21:06 -0500

    Complete a stream based on another stream with takeUntil

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,34 @@
-import { interval, fromEvent } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { interval, fromEvent, Observable } from 'rxjs';
+import { takeUntil, mapTo, scan, takeWhile } from 'rxjs/operators';
 
+// elements
+const countdown = document.getElementById('countdown');
+const message = document.getElementById('message');
+const abortButton = document.getElementById('abort');
+
+// streams
 const counter$ = interval(1000);
-const click$ = fromEvent(document, 'click');
+let abortClick$: Observable<Event>;
+if (abortButton instanceof HTMLElement) {
+  abortClick$ = fromEvent(abortButton, 'click');
+} else {
+  abortClick$ = fromEvent(document, 'click');
+}
 
-counter$.pipe(takeUntil(click$)).subscribe(console.log);
+counter$
+  .pipe(
+    mapTo(-1),
+    scan((accumulator, current) => {
+      return accumulator + current;
+    }, 5),
+    takeWhile(value => value >= 0),
+    takeUntil(abortClick$),
+  )
+  .subscribe(value => {
+    if (countdown instanceof HTMLElement) {
+      countdown.innerHTML = value.toString();
+    }
+    if (!value && message instanceof HTMLElement) {
+      message.innerHTML = 'Liftoff!';
+    }
+  });

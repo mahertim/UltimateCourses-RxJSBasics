91cde106c34d2f061f00d3faeeb96d430349d6bf
Tim Maher <tim@timmaher.dev>
2019-12-31 12:02:49 -0500

    Solo Lab 1: debounce, throttle, and sample comparison

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
-import { Observable, fromEvent, interval } from 'rxjs';
-import { sample } from 'rxjs/operators';
+import { interval } from 'rxjs';
+import { sampleTime, throttleTime, debounceTime } from 'rxjs/operators';
+
+// elements
+const timer = document.getElementById('timer') as HTMLElement;
+const debouncer = document.getElementById('debouncer') as HTMLElement;
+const throttler = document.getElementById('throttler') as HTMLElement;
+const sampler = document.getElementById('sampler') as HTMLElement;
 
 // streams
-const click$ = fromEvent(document, 'click') as Observable<MouseEvent>;
 const timer$ = interval(1000);
+const debouncer$ = timer$.pipe(debounceTime(500));
+const throttler$ = timer$.pipe(throttleTime(2000));
+const sampler$ = timer$.pipe(sampleTime(2000));
 
-timer$.pipe(sample(click$)).subscribe(console.log);
+timer$.subscribe(tick => {
+  timer.innerHTML = `Timer: ${tick}`;
+});
+debouncer$.subscribe(tick => {
+  debouncer.innerHTML = `Debouncer: ${tick}`;
+});
+throttler$.subscribe(tick => {
+  throttler.innerHTML = `Throttler: ${tick}`;
+});
+sampler$.subscribe(tick => {
+  sampler.innerHTML = `Sampler: ${tick}`;
+});

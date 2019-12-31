328c0f7cdca83309cc2ae51c453ab7533ee02221
Tim Maher <tim@timmaher.dev>
2019-12-31 09:38:37 -0500

    Ignore non-unique values with distinctUntilChanged

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
-import { Observable, of } from 'rxjs';
-import { distinctUntilChanged } from 'rxjs/operators';
+import { from } from 'rxjs';
+import { scan, map, distinctUntilKeyChanged } from 'rxjs/operators';
 
-const numbers$: Observable<number> = of(1, 1, 2, 2, 2, 3, 3, 4, 5, 5, 3);
+class User {
+  constructor(
+    public name: string = '',
+    public loggedIn: boolean = false,
+    public token: string | null = null,
+  ) {}
+}
 
-numbers$
-  .pipe(
-    // uses === by default
-    distinctUntilChanged(),
-  )
-  .subscribe(console.log);
+const user = [
+  new User('Brian', false, null),
+  new User('Brian', true, 'abc'),
+  new User('Brian', true, '123'),
+];
+
+const state$ = from(user).pipe(
+  scan((accumulator, currentValue) => {
+    return { ...accumulator, ...currentValue };
+  }, new User()),
+);
+
+const name$ = state$.pipe(
+  distinctUntilKeyChanged('name'),
+  map((state: any) => state.name),
+);
+
+name$.subscribe(console.log);

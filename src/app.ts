bc62c35f552e8684dd83512a9cb834d8702073c3
Tim Maher <tim@timmaher.dev>
2019-12-31 11:46:51 -0500

    Basic sampleTime

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,12 @@
-import { fromEvent, asyncScheduler } from 'rxjs';
-import { map, tap, throttleTime } from 'rxjs/operators';
-
-// helpers
-function calculateScrollPercent(element: {
-  scrollTop: any;
-  scrollHeight: any;
-  clientHeight: any;
-}) {
-  const { scrollTop, scrollHeight, clientHeight } = element;
-
-  return (scrollTop / (scrollHeight - clientHeight)) * 100;
-}
-
-// elements
-const progressBar = document.querySelector('.progress-bar') as HTMLElement;
+import { Observable, fromEvent } from 'rxjs';
+import { sampleTime, map } from 'rxjs/operators';
 
 // streams
-const scroll$ = fromEvent(document, 'scroll');
-const progress$ = scroll$.pipe(
-  throttleTime(30, asyncScheduler, {
-    leading: false,
-    trailing: true,
-  }),
-  map(({ target }: any) => calculateScrollPercent(target.documentElement)),
-  tap(console.log),
-);
+const click$ = fromEvent(document, 'click') as Observable<MouseEvent>;
 
-progress$.subscribe(percent => {
-  progressBar.style.width = `${percent}%`;
-});
+click$
+  .pipe(
+    sampleTime(4000),
+    map(({ clientX, clientY }) => ({ clientX, clientY })),
+  )
+  .subscribe(console.log);

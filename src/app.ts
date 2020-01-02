7526bdea5e5c6bc8bbea2e387c02c950fa86c9cd
Tim Maher <tim@timmaher.dev>
2020-01-02 10:42:30 -0500

    Basic mergeMap

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-import { fromEvent, asyncScheduler } from 'rxjs';
-import { auditTime, throttleTime } from 'rxjs/operators';
+import { fromEvent, interval } from 'rxjs';
+import { mergeMap, takeUntil } from 'rxjs/operators';
 
 // streams
-const click$ = fromEvent(document, 'click');
+// const click$ = fromEvent(document, 'click');
+const mousedown$ = fromEvent(document, 'mousedown');
+const mouseup$ = fromEvent(document, 'mouseup');
+const interval$ = interval(1000);
+const betterInterval = interval$.pipe(takeUntil(mouseup$));
 
-// these act the same
-// auditTime(500) == throttleTime(500, asyncScheduler, { leading: false, trailing: true })
-click$
-  .pipe(throttleTime(4000, asyncScheduler, { leading: false, trailing: true }))
-  .subscribe(console.log);
-click$.pipe(auditTime(4000)).subscribe(console.log);
+mousedown$.pipe(mergeMap(() => betterInterval)).subscribe(console.log);

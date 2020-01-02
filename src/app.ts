9eb4a495e812d0295de717466bad3eae6df79ffd
Tim Maher <tim@timmaher.dev>
2020-01-02 15:56:36 -0500

    Queue observable execution using concat

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,16 @@
-import { interval, fromEvent } from 'rxjs';
-import { scan, mapTo, takeWhile, takeUntil, startWith } from 'rxjs/operators';
+import { empty } from 'rxjs';
+import { concat, startWith, delay } from 'rxjs/operators';
 
-// elem refs
-const countdown = document.getElementById('countdown');
-const message = document.getElementById('message');
-const abortButton = document.getElementById('abort');
+const delayed$ = empty().pipe(delay(1000));
 
-// streams
-const counter$ = interval(1000);
-const abort$ = fromEvent(abortButton, 'click');
-
-const COUNTDOWN_FROM = 20;
-
-counter$
+delayed$
   .pipe(
-    mapTo(-1),
-    scan((accumulator, current) => {
-      return accumulator + current;
-    }, COUNTDOWN_FROM),
-    takeWhile(value => value >= 0),
-    takeUntil(abort$),
-    startWith(COUNTDOWN_FROM),
+    concat(
+      delayed$.pipe(startWith('3...')),
+      delayed$.pipe(startWith('2...')),
+      delayed$.pipe(startWith('1...')),
+      delayed$.pipe(startWith('GO!')),
+    ),
+    startWith('Get Ready!'),
   )
-  .subscribe((value: any) => {
-    countdown.innerHTML = value;
-    if (!value) {
-      message.innerHTML = 'Liftoff!';
-    }
-  });
+  .subscribe(console.log);

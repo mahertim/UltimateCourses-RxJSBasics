1fe39fcc25e7e8fc0b146d42731800289e696bdb
Tim Maher <tim@timmaher.dev>
2020-01-02 16:14:04 -0500

    Combine multiple active observables using merge

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,34 @@
-import { fromEvent, merge } from 'rxjs';
-// import { concat, startWith, delay } from 'rxjs/operators';
+import { interval, fromEvent, merge, empty } from 'rxjs';
+import { scan, mapTo, takeWhile, startWith, switchMap } from 'rxjs/operators';
 
-const keyup$ = fromEvent(document, 'keyup');
-const click$ = fromEvent(document, 'click');
+// elem refs
+const countdown = document.getElementById('countdown');
+const message = document.getElementById('message');
+const pauseButton = document.getElementById('pause');
+const startButton = document.getElementById('start');
 
-// keyup$.subscribe(console.log);
-// click$.subscribe(console.log);
+// streams
+const counter$ = interval(1000);
+const pauseClick$ = fromEvent(pauseButton, 'click');
+const startClick$ = fromEvent(startButton, 'click');
 
-merge(keyup$, click$).subscribe(console.log);
+const COUNTDOWN_FROM = 20;
+
+merge(startClick$.pipe(mapTo(true)), pauseClick$.pipe(mapTo(false)))
+  .pipe(
+    switchMap(shouldStart => {
+      return shouldStart ? counter$ : empty();
+    }),
+    mapTo(-1),
+    scan((accumulator, current) => {
+      return accumulator + current;
+    }, COUNTDOWN_FROM),
+    takeWhile(value => value >= 0),
+    startWith(COUNTDOWN_FROM),
+  )
+  .subscribe((value: any) => {
+    countdown.innerHTML = value;
+    if (!value) {
+      message.innerHTML = 'Liftoff!';
+    }
+  });

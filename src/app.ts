4930b1de9b43dbb7699006d4061880fef8dfd10e
Tim Maher <tim@timmaher.dev>
2020-01-02 11:18:32 -0500

    Switch to a new observable on emissions using switchMap

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,34 @@
-import { fromEvent, interval, Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import {
+  debounceTime,
+  pluck,
+  distinctUntilChanged,
+  switchMap,
+} from 'rxjs/operators';
+
+const BASE_URL = 'https://api.openbrewerydb.org/breweries';
+const typeaheadContainer = document.getElementById(
+  'typeahead-container',
+) as HTMLElement;
+
+// elements
+const inputBox = document.getElementById('text-input') as HTMLElement;
 
 // streams
-const interval$ = interval(1000);
-const click$ = fromEvent(document, 'click') as Observable<MouseEvent>;
+const input$ = fromEvent(inputBox, 'keyup');
 
-click$.pipe(switchMap(() => interval$)).subscribe(console.log);
+input$
+  .pipe(
+    debounceTime(200),
+    pluck('target', 'value'),
+    distinctUntilChanged(),
+    switchMap(searchTerm => {
+      return ajax.getJSON(`${BASE_URL}?by_name=${searchTerm}`);
+    }),
+  )
+  .subscribe((response: any[]) => {
+    typeaheadContainer.innerHTML = response
+      .map((b: { name: any }) => b.name)
+      .join('<br>');
+  });

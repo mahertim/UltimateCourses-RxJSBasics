65461fb562350720244fb8f8bde34a188a6b6758
Tim Maher <tim@timmaher.dev>
2020-01-02 16:44:23 -0500

    Basic forkJoin

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,8 @@
-import { fromEvent, combineLatest, interval } from 'rxjs';
-import { map, filter, withLatestFrom } from 'rxjs/operators';
-
-// helpers
-const keyupAsValue = (elem: HTMLInputElement) => {
-  return fromEvent(elem, 'keyup').pipe(
-    map(
-      (event: KeyboardEvent) =>
-        (event.target as HTMLInputElement).valueAsNumber,
-    ),
-  );
-};
-
-// elements
-const first = document.getElementById('first') as HTMLInputElement;
-const second = document.getElementById('second') as HTMLInputElement;
+import { of, forkJoin } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 // streams
-const click$ = fromEvent(document, 'click');
+const numbers$ = of(1, 2, 3);
+const letters$ = of('a', 'b', 'c');
 
-click$.pipe(withLatestFrom(interval(1000))).subscribe(console.log);
-
-combineLatest(keyupAsValue(first), keyupAsValue(second))
-  .pipe(
-    filter(([first, second]) => {
-      return !isNaN(first) && !isNaN(second);
-    }),
-    map(([first, second]) => {
-      return first + second;
-    }),
-  )
-  .subscribe(console.log);
+forkJoin(numbers$, letters$.pipe(delay(3000))).subscribe(console.log);
